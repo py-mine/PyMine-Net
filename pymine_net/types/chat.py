946d0a669e2a876a4014c1574d94ce60d61d5200
Milo Weinberg <iapetus011@gmail.com>
2022-02-17 19:59:16 -0500

    add chat type

---
+++ pymine_net/types/chat.py
@@ -0,0 +1,10 @@
+
+from typing import Union
+
+
+class Chat:
+    def __init__(self, chat: Union[str, dict]):
+        if isinstance(chat, str):
+            self.chat = {"text": chat}
+        else:
+            self.chat = chat
