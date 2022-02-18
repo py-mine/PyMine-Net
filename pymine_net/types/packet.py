4d5fb459eb2b0554e656b92c47c7edb3358dd89f
Milo Weinberg <iapetus011@gmail.com>
2022-02-18 15:09:58 -0500

    add + use strict abstract classes

--- pymine_net/types/packet.py
+++ pymine_net/types/packet.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod, abstractclassmethod
+from pymine_net.strict_abc import StrictABC, abstract
 
 from pymine_net.types.buffer import Buffer
 
 __all__ = ("Packet", "ServerBoundPacket", "ClientBoundPacket")
 
 
-class Packet(ABC):
+class Packet(StrictABC):
     """Base Packet class.
 
-    :param int id: Packet identifaction number. Defaults to None.
+    :param int id: Packet identification number. Defaults to None.
     :ivar id:
     """
 
@@ -21,11 +21,12 @@ class Packet(ABC):
 class ServerBoundPacket(Packet):
     """Base Packet class for packets received from the client. (Client -> Server)
 
-    :param int id: Packet identifaction number. Defaults to None.
+    :param int id: Packet identification number. Defaults to None.
     :ivar id:
     """
 
-    @abstractclassmethod
+    @classmethod
+    @abstract
     def unpack(cls, buf: Buffer) -> ServerBoundPacket:
         pass
 
@@ -33,10 +34,10 @@ class ServerBoundPacket(Packet):
 class ClientBoundPacket(Packet):
     """Base Packet class for packets to be sent from the server. (Server -> Client)
 
-    :param int id: Packet identifaction number. Defaults to None.
+    :param int id: Packet identification number. Defaults to None.
     :ivar id:
     """
 
-    @abstractmethod
+    @abstract
     def pack(self) -> Buffer:
         pass
