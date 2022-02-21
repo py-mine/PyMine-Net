fe643dbf43baff86cd14e46db2bdeceec79d7f3e
Sh-wayz <shwayz03@gmail.com>
2022-02-21 08:27:54 -0500

    0x21

---
+++ pymine_net/packets/757/play/keep_alive.py
@@ -0,0 +1,52 @@
+"""Contains packets for maintaining the connection between client and server."""
+
+from __future__ import annotations
+
+from pymine_net.types.packet import ServerBoundPacket, ClientBoundPacket
+from pymine_net.types.buffer import Buffer
+
+__all__ = (
+    "PlayKeepAliveClientBound",
+    "PlayKeepAliveServerBound",
+)
+
+
+class PlayKeepAliveClientBound(ClientBoundPacket):
+    """Sent by the server in order to maintain connection with the client. (Server -> Client)
+
+    :param int keep_alive_id: A randomly generated (by the server) integer/long.
+    :ivar int id: Unique packet ID.
+    :ivar keep_alive_id:
+    """
+
+    id = 0x21
+
+    def __init__(self, keep_alive_id: int) -> None:
+        super().__init__()
+
+        self.keep_alive_id = keep_alive_id
+
+    def pack(self) -> Buffer:
+        return Buffer().write("q", self.keep_alive_id)
+
+
+class PlayKeepAliveServerBound(Packet):
+    """Sent by client in order to maintain connection with server. (Client -> Server)
+
+    :param int keep_alive_id: A randomly generated (by the server) integer/long.
+    :ivar int id: Unique packet ID.
+    :ivar int to: Packet direction.
+    :ivar keep_alive_id:
+    """
+
+    id = 0x10
+    to = 0
+
+    def __init__(self, keep_alive_id: int) -> None:
+        super().__init__()
+
+        self.keep_alive_id = keep_alive_id
+
+    @classmethod
+    def decode(cls, buf: Buffer) -> PlayKeepAliveServerBound:
+        return cls(buf.unpack("q"))
