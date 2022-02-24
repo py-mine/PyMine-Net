67e1fbb5bb58472ae1c1f5abc98a1316737c1728
Sh-wayz <shwayz03@gmail.com>
2022-02-24 18:49:01 -0500

    0x18

---
+++ pymine_net/packets/v_1_18_1/play/plugin_msg.py
@@ -0,0 +1,52 @@
+"""Contains packets relating to plugin channels and messages. See here: https://wiki.vg/Plugin_channels"""
+
+from __future__ import annotations
+
+from pymine_net.types.packet import ClientBoundPacket, ServerBoundPacket
+from pymine_net.types.buffer import Buffer
+
+__all__ = ("PlayPluginMessageClientBound", "PlayPluginMessageServerBound")
+
+
+class PlayPluginMessageClientBound(ClientBoundPacket):
+    """Used to send a "plugin message". See here https://wiki.vg/Protocol#Plugin_Message_.28serverbound.29 (Server -> Client)
+
+    :param str channel: The plugin channel to be used.
+    :param bytes data: Data to be sent to the client.
+    :ivar int id: Unique packet ID.
+    :ivar data:
+    """
+
+    id = 0x18
+
+    def __init__(self, channel: str, data: bytes) -> None:
+        super().__init__()
+
+        self.channel = channel
+        self.data = data
+
+    def pack(self) -> Buffer:
+        return Buffer().write_string("self.channel").write_bytes(self.data )
+
+
+class PlayPluginMessageServerBound(Packet):
+    """Used to send plugin data to the server (Client -> Server)
+
+    :param str channel: The plugin channel being used.
+    :param bytes data: Data to be sent to the client.
+    :ivar int id: Unique packet ID.
+    :ivar int to: Packet direction.
+    :ivar data:
+    """
+
+    id = 0x0B
+    to = 0
+
+    def __init__(self, channel: str, data: bytes) -> None:
+        super().__init__()
+
+        self.channel = channel
+        self.data = data
+
+    def decode(self, buf: Buffer) -> PlayPluginMessageServerBound:
+        return PlayPluginMessageServerBound(buf.unpack_string(), Buffer(buf.read()))
