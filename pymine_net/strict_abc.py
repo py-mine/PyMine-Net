d6d7be72a219e595670d4f47ae0f7a8f9b5371ac
ItsDrike <itsdrike@protonmail.com>
2022-02-26 18:09:08 +0100

    Rework strict ABC class This completely changes the implementation of the strict ABC. Instead of the previous approach, this relies on inheriting the default behavior of `abc.ABCMeta` and just extending it, there's no need to reinvent the wheel here. This results in a much smaller and perhaps cleaner implementation.
    This also changes the way type-hints are compared, by using
    inspect.get_annotations with eval_str=True, allowing us to evaluate the
    string forward references immediately, for comparison. This then allows
    us to use different strings, if the same object was imported
    differently, or using subclasses.

--- pymine_net/strict_abc.py
+++ pymine_net/strict_abc.py
@@ -1,124 +1,133 @@
 """
-Contains a "strict" implementation of abc.ABC, as only Python 3.10+ actually enforces abc.ABC.
+Contains a "strict" implementation of abc.ABC, enforcing presnce of all abstract methods on class creation.
 
 - Subclasses of abstract classes must implement all abstract methods/classmethods/staticmethods
 - A method's typehints are also enforced, if abstract method a has a return annotation of MyClass,
     the implemented method must have a return annotation of MyClass or a subclass of MyClass.
 """
+from __future__ import annotations
 
-from typing import Optional, Tuple, Type
+import inspect
+from abc import ABCMeta
+from typing import Tuple, List, Callable, Type, TYPE_CHECKING
 
-__all__ = ("abstract", "StrictABC")
-
-
-class AbstractObjData:
-    __slots__ = ("optional", "annotations")
-
-    def __init__(self, optional: bool = False, annotations: dict = None):
-        self.optional = optional
-        self.annotations = annotations
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
-def abstract(obj):
-    obj.__abstract__ = AbstractObjData(annotations=getattr(obj, "__annotations__", None))
-    return obj
+
+__all__ = ("StrictABC", )
 
 
-def optionalabstract(obj):
-    obj.__abstract__ = AbstractObjData(
-        optional=True, annotations=getattr(obj, "__annotations__", None)
-    )
-    return obj
+class StrictABCMeta(ABCMeta):
+    """Strict extension of abc.ABCMeta, enforcing abstract methods on definitions and typing checks.
 
+    Regular abc.ABCMeta only enforces abstract methods to be present on class initialization,
+    however, this often isn't enough, because we usually want to get runtime errors immediately,
+    whenever a class  would be defined without implementing all of the abstract methods. With
+    behavior like this, we can ensure that we encounter a runtime failure before this behavior ever
+    gets a chance of affecting things, this is especially important for static and class methods which
+    don't need initialization to be called.
 
-def is_abstract(obj) -> bool:
-    return hasattr(obj, "__abstract__")
+    This class also introduces typing checks, which require the overridden methods to follow the
+    type-hints specified in the initial abstract methods. This prevents bad unexpected behavior,
+    and it enforces clean type checkable code."""
 
-
-def check_annotations(a: dict, b: dict) -> bool:
-    for k, v in a.items():
-        if k not in b:
-            return False
-
-        if type(v) is str:
-            return type(b[k]) is str and v == b[k]
-
-        try:
-            if not issubclass(b[k], v):
-                return False
-        except TypeError:
-            if b[k] is not v:
-                return False
-
-    return True
-
-
-class UnimplementedAbstractError(Exception):
-    def __init__(self, cls_name: str, obj_name: str):
-        super().__init__(f"Class {cls_name} didn't implement abstract {obj_name}.")
-
-
-class MismatchedOverridenAbstractError(Exception):
-    def __init__(
-        self, cls_name: str, obj_name: str, abstract_annotations: dict, obj_annotations: dict
+    def __new__(
+        mcls,
+        name: str,
+        bases: Tuple[Type[object], ...],
+        dct: dict[str, object],
+        definition_check: bool = True,
+        typing_check: bool = True,
     ):
-        super().__init__(
-            f"Overriden abstract {obj_name} of {cls_name} has mismatched annotations: {obj_name}({self.fmt_annos(abstract_annotations)}) != {obj_name}({self.fmt_annos(obj_annotations)})."
-        )
+        cls = super().__new__(mcls, name, bases, dct)
+
+        if definition_check and len(cls.__abstractmethods__) > 0:
+            missing_methods = ", ".join(cls.__abstractmethods__)
+            raise TypeError(f"Can't define abstract class Bar with unimplemented abstract methods: {missing_methods}")
+        if typing_check:
+            abc_classes = []
+            for base_cls in bases:
+                if isinstance(base_cls, mcls) and base_cls is not mcls:
+                    abc_classes.append(base_cls)
+
+            mcls._check_annotations(cls, abc_classes)
+
+        return cls
+
+    @classmethod
+    def _check_annotations(mcls, cls: Self, abc_classes: List[Self]) -> None:
+        """Make sure all overidden methods in the class match the original definitions in ABCs.
+
+        This works by finding every abstract method in the ABC classes (present in the classe's MRO),
+        getting their type annotations and comparing them with the annotations in the overridden methods.
+
+        If an abstract method definition is found, but overridden implementation in the class isn't present,
+        this method will be skipped."""
+        # Get all of the defined abstract methods in the ABCs
+        abc_methods = []
+        for abc_cls in abc_classes:
+            for abc_method_name in abc_cls.__abstractmethods__:
+                abc_method = getattr(abc_cls, abc_method_name)
+                if not callable(abc_method):
+                    raise TypeError(f"Expected '{abc_method_name}' to be an abstractmethod, but it isn't callable.")
+                abc_methods.append((abc_method_name, abc_method))
+
+        # Get matching overridden methods in the class, to the collected abstract methods
+        _MISSING_SENTINEL = object()
+        for abc_method_name, abc_method in abc_methods:
+            defined_method = getattr(cls, abc_method_name, _MISSING_SENTINEL)
+
+            if defined_method is _MISSING_SENTINEL:
+                continue  # Skip unoverridden abstract methods
+
+            if not callable(defined_method):
+                raise TypeError(f"Expected '{abc_method_name}' to be an abstractmethod, but it isn't callable.")
+
+            # Compare the annotations
+            mcls._compare_annotations(abc_method, defined_method, abc_method_name)
 
     @staticmethod
-    def fmt_annos(dct: dict) -> str:
-        return ",".join(f"{k}:{getattr(v, '__name__', str(v))}" for k, v in dct.items())
+    def _compare_annotations(expected_f: Callable, compare_f: Callable, method_name: str) -> None:
+        """Compare annotations between two given functions.
 
+        - If the first (expected) function doesn't have any annotations, this check is skipped.
+        - If the second (compare) function doesn't have any annotations, check fails (TypeError).
+        - If the second (compare) function's signature isn't compatible the expected signature, check fails (TypeError).
+        - Otherwise, if the signatures are compatible, the check is passed (returns None).
+        """
+        expected_ann = inspect.get_annotations(expected_f, eval_str=True)
+        compare_ann = inspect.get_annotations(compare_f, eval_str=True)
+        err_msg = f"Mismatched annotations in '{method_name}'."
 
-class StrictABCMeta(type):
-    def __new__(cls, name: str, bases: Tuple[Type[object]], dct: dict):
-        self = super().__new__(cls, name, bases, dct)
+        if len(expected_ann) == 0:  # Nothing to compare
+            return
 
-        # iterate through base classes
-        for base in bases:
-            # iterate through objects
-            for obj_name in dir(base):
-                obj = getattr(base, obj_name)
+        if len(compare_ann) == 0:
+            raise TypeError(err_msg + f" Compare method has no annotations, but some were expected ({expected_ann}).")
 
-                abstract_data: Optional[AbstractObjData] = getattr(obj, "__abstract__", None)
+        for key, exp_val in expected_ann.items():
+            if key not in compare_ann:
+                raise TypeError(err_msg + f" Annotation for '{key}' not present, should be {exp_val}.")
 
-                self_obj = getattr(self, obj_name, None)
+            cmp_val = compare_ann[key]
 
-                # check if it's an abstract object
-                if abstract_data is not None:
-                    # check to see if this class implements that abstract object
-                    if hasattr(self_obj, "__abstract__") and not abstract_data.optional:
-                        raise UnimplementedAbstractError(self.__name__, obj.__name__)
-
-                    if type(self_obj) is not type(obj) and not abstract_data.optional:
-                        raise UnimplementedAbstractError(self.__name__, obj.__name__)
-
-                    # check annotations if there are any
-                    if abstract_data.annotations is not None:
-                        self_obj_annotations = getattr(self_obj, "__annotations__", None)
-
-                        if self_obj_annotations is None and abstract_data.annotations is not None:
-                            raise MismatchedOverridenAbstractError(
-                                self.__name__,
-                                obj.__name__,
-                                abstract_data.annotations,
-                                self_obj_annotations,
-                            )
-
-                        if not check_annotations(abstract_data.annotations, self_obj_annotations):
-                            raise MismatchedOverridenAbstractError(
-                                self.__name__,
-                                obj.__name__,
-                                abstract_data.annotations,
-                                self_obj_annotations,
-                            )
-
-        return self
+            try:
+                if not issubclass(cmp_val, exp_val):
+                    raise TypeError(err_msg + f" Annotation for '{key}' isn't compatible, should be {exp_val}, got {cmp_val}.")
+            except TypeError:
+                # This can happen when cmp_val isn't a class, for example with it set to `None`
+                # Do a literal 'is' comparison here when that happens
+                if cmp_val is not exp_val:
+                    raise TypeError(err_msg + f" Annotation for '{key} isn't compatible, should be {exp_val}, got {cmp_val}.")
 
 
 class StrictABC(metaclass=StrictABCMeta):
-    pass
+    """Strict implementation of abc.ABC, including definition time checks and typing checks.
+
+    For more info, check StrictABCMeta's doocstring."""
+    __slots__ = ()
 
 
 # Example:
