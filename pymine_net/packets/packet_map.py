f62d4ed36a25416af30f3296d2c22e11f3e7b6d9
ItsDrike <itsdrike@protonmail.com>
2022-03-05 10:20:27 +0100

    Rework load_packet_map logic

--- pymine_net/packets/packet_map.py
+++ pymine_net/packets/packet_map.py
@@ -1,7 +1,7 @@
-import importlib
+import pkgutil
 import warnings
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Dict, Iterable, Union, NoReturn, Type
 
 from pymine_net.enums import GameState
 from pymine_net.types.packet import Packet
@@ -11,58 +11,76 @@ __all__ = ("load_packet_map",)
 
 
 # the directory this file is contained in
-FILE_DIR = Path(__file__).parent.absolute()
+FILE_DIR = Path(__file__).parent
 
 PROTOCOL_MAP = {757: "v_1_18_1"}
 
 
+def walk_packet_classes(
+    protocol_name: str,
+    state: GameState,
+    debug: bool = False
+) -> Iterable[Type[Packet]]:
+    """
+    Try to import every pymine.packets.{protocol_name}.{state.name} package,
+    and search it's __all__ for presence of subclasses of the Packet class.
+    When a subclass is found, yield it.
+    """
+    # Directories in the package under protocol_name should always contain
+    # all of the GameState's enum names, as subpackages. So we can assume
+    # that this path exists, and if not, it will result in an ImportError
+    # later.
+    path = str(Path(FILE_DIR, protocol_name, state.name).absolute())
+    prefix = f"pymine_net.packets.{protocol_name}.{state.name}"
+
+    def on_error(name: str) -> NoReturn:
+        raise ImportError(name=name)
+
+    for module in pkgutil.walk_packages(path, prefix, onerror=on_error):
+        if debug and not hasattr(module, "__all__"):
+            warnings.warn(f"{module.__name__} is missing member __all__ and cannot be loaded.")
+            continue
+
+        for member_name in module.__all__:
+            try:
+                obj = getattr(module, member_name)
+            except AttributeError:
+                if debug:
+                    warnings.warn(
+                        f"{module.__name__} is missing member {member_name!r} present in __all__."
+                    )
+            else:
+                if issubclass(obj, Packet):
+                    yield obj
+
+
 def load_packet_map(protocol: Union[int, str], *, debug: bool = False) -> PacketMap:
     """
-    Iterates through the protocol's packets direction and constructs a PacketMap.
-    - Used for automatic decoding of incoming packet ids into packet classes for decoding the entire packet.
-    - Also necessary for the packet tests to function.
+    Collects all packet classes for each GameState for given protocol to construct a PacketMap.
+
+    - If passed protocol is an `int`, we treat it as a version number and use the corresponding
+    protocol import name defined in PROTOCOL_MAP.
+    - If it's a `str`, we treat it as the import name directly.
+
+    This is useful for automatic decoding of incoming packet ids into packet classes for decoding
+    the entire packet.
     """
 
-    protocol = PROTOCOL_MAP.get(protocol, protocol)
+    if isinstance(protocol, int):
+        # If given protocol was int (version) and it isn't present in
+        # the protocol map, we end with ha KeyError since we don't know
+        # what to import.
+        protocol = PROTOCOL_MAP[protocol]
+
     packets: Dict[GameState, StatePacketMap] = {}
 
-    for state_name, state in GameState.__members__.items():
-        module_base = [str(protocol), state_name]
-
-        packet_classes: List[Packet] = []
-
-        # iterate through the files in the <state> folder
-        for path in Path(FILE_DIR, *module_base).iterdir():
-            if not path.is_file() or not path.name.endswith(".py"):
-                continue
-
-            # import the file (pymine_net.packets.<protocol>.<state>.<packet group>)
-            module = importlib.import_module(
-                ".".join(["pymine_net", "packets", *module_base, path.name[:-3]])
-            )
-
-            if debug and not hasattr(module, "__all__"):
-                warnings.warn(f"{module.__name__} is missing member __all__ and cannot be loaded.")
-                continue
-
-            # iterate through object names in module.__all__
-            for member_name in module.__all__:
-                # attempt to get object, warn if it's not actually present
-                try:
-                    obj = getattr(module, member_name)
-                except AttributeError:
-                    if debug:
-                        warnings.warn(
-                            f"{module.__name__} is missing member {member_name!r} present in __all__."
-                        )
-                else:
-                    if issubclass(obj, Packet):
-                        packet_classes.append(obj)
+    for state in GameState:
+        packet_classes = [packet_class for packet_class in walk_packet_classes(protocol, state, debug)]
 
         # attempt to create the StatePacketMap from the list of loaded packets.
         try:
             packets[state] = StatePacketMap.from_list(state, packet_classes, check_duplicates=debug)
-        except DuplicatePacketIdError as e:  # re-raise with protocol included in exception
-            raise DuplicatePacketIdError(protocol, e.state, e.packet_id, e.direction)
+        except DuplicatePacketIdError as exc:  # re-raise with protocol included in exception
+            raise DuplicatePacketIdError(protocol, exc.state, exc.packet_id, exc.direction)
 
     return PacketMap(protocol, packets)
