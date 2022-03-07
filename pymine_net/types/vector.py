7dde19684ce35e89cd798be6b4ef69a3089f7127
ItsDrike <itsdrike@protonmail.com>
2022-03-07 22:32:18 +0100

    Rename bound TypeVar to more specific name

--- pymine_net/types/vector.py
+++ pymine_net/types/vector.py
@@ -1,28 +1,28 @@
 from typing import Generic, TypeVar
 
-T = TypeVar("T", bound=float)
+T_Numeric = TypeVar("T_Numeric", bound=float)
 
 
-class Vector3(Generic[T]):
+class Vector3(Generic[T_Numeric]):
     """
     Stores three numeric values: x, y, z.
     - Used for position and movement data in the Player class.
     """
     __slots__ = ("x", "y", "z")
 
-    def __init__(self, x: T, y: T, z: T):
+    def __init__(self, x: T_Numeric, y: T_Numeric, z: T_Numeric):
         self.x = x
         self.y = y
         self.z = z
 
 
-class Rotation(Generic[T]):
+class Rotation(Generic[T_Numeric]):
     """
     Stores the pitch and yaw values of a rotation.
     - Used for storing rotation data in the Player class.
     """
     __slots__ = ("yaw", "pitch")
 
-    def __init__(self, yaw: T, pitch: T):
+    def __init__(self, yaw: T_Numeric, pitch: T_Numeric):
         self.yaw = yaw
         self.pitch = pitch
