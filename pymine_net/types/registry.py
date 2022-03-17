57ff2f001959657bb16f8ffa6f613b7aefc73d6c
ItsDrike <itsdrike@protonmail.com>
2022-03-17 14:20:39 +0100

    Split logic for taking sequences to a classmethod Accepting both a mapping and a sequence from init as data is weird, it just makes the code more messy and it's not a necessary thing that needs to be supported by the main constructor, it's an alternative way to pass data over, which means it should use an alternative constructor.
    This adds `from_sequence` classmethod constructor to address this.

--- pymine_net/types/registry.py
+++ pymine_net/types/registry.py
@@ -1,4 +1,7 @@
-from typing import Any, Mapping, Sequence, Union
+from typing import Any, Mapping, Sequence, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 __all__ = ("Registry",)
 
@@ -8,24 +11,20 @@ class Registry:
 
     def __init__(
         self,
-        data: Union[Mapping, Sequence],
-        data_reversed: Mapping = None,
+        data: Mapping,
+        data_reversed: Optional[Mapping] = None,
     ):
         """
         Makes a doubly hashed map, providing O(1) lookups for both keys and values.
 
         If data_reversed is specified, we use it for the reverse map instead of autogenerating
         one from data  by simply swapping keys and values.
-
-        If data is given as an iterable, we treat the positions as IDs (keys) and the elements as values.
         """
         if data_reversed is not None and not isinstance(data_reversed, Mapping):
             raise TypeError(f"data_reversed must be a Mapping, got {type(data_reversed)}.")
 
         if isinstance(data, Mapping):
             self.data = dict(data)
-        elif isinstance(data, Sequence):
-            self.data = {v: i for i, v in enumerate(data)}
         else:
             raise TypeError(f"Can't make registry from {type(data)}, must be Sequence/Mapping.")
 
@@ -34,6 +33,17 @@ class Registry:
             self.data_reversed = {v: k for k, v in self.data.items()}
         else:
             self.data_reversed = data_reversed
+
+    @classmethod
+    def from_sequence(cls, seq: Sequence, reversed_data: Optional[Mapping] = None) -> Self:
+        """
+        Initialize the registry from a sequence, using it's positions (indices)
+        as the values (often useful with things like numeric block IDs, etc.)
+        and it's elements as the keys.
+
+        If reversed_data is passed, we simply pass it over to __init__.
+        """
+        return cls({v: i for i, v in enumerate(seq)}, reversed_data)
 
     def encode(self, key: Any) -> Any:
         """Key -> value, most likely an identifier to an integer."""
